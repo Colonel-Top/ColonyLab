69498c9a1ca8b5ff9e75cd4d0059106c219601fe
Colonel-Top <promsurinm@hotmail.com>
2018-01-11 23:36:17 +0700

    Add Python

---
+++ storage/runtime.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+from threading import Timer
+import time
+import sys
+import os
+import subprocess
+def kill(p):
+    try:
+        p.kill()
+    except OSError:
+        pass # ignore
+    
+def main():
+    path =  str(sys.argv[1])
+    #path = "HI"
+    MAX_RUNTIME = 3
+    FREQ = 1
+    flag = False
+    #execution = threading.Thread(target = runner,args = (MAX_RUNTIME,FREQ))
+    #execution.start()
+    proc = subprocess.Popen(path,shell=True)
+
+    while True:
+        if proc.poll() is None:
+            time.sleep(FREQ)
+            count = count + FREQ
+            print(count)
+            if count > MAX_RUNTIME:
+                proc.kill()
+                print("NOT OK")
+                break
+        if proc.poll() != None:
+            print("OK")
+            return 0
+    print("FINISH")
+    return 1'''
+
+if __name__ == "__main__":
+    main()
---
+++ storage/test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class test
+{
+	public static void main (String[] args)
+{
+	/*int a = 0;
+	int b = 2;
+	int c=  a+b;*/
+	Scanner a = new Scanner(System.in);
+	int x1 = a.nextInt();
+	int s = a.nextInt();
+	int x2 = 0;
+	
+	
+		x2 = 2*s-x1;
+		System.out.print(x2);
+		//System.out.print("Test");
+	
+}
+}
