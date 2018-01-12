87388fe91989d4e1406a4bfd6979870bec499789
Colonel-Top <promsurinm@hotmail.com>
2018-01-12 18:00:50 +0700

    Add ALL

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -43,7 +43,7 @@
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                             <label for="confirm" class="col-md-4 control-label">Confirm Course Password</label>
                             <div class="col-md-6">
-                                <input id="confirm" type="password" class="form-control" name="password_confirmation">
+                                <input id="confirm" type="password" class="form-control" name="confirm">
 
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
