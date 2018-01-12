2f6337120a855c854e9e009772deb44f6df176bd
Colonel-Top <promsurinm@hotmail.com>
2018-01-12 20:38:02 +0700

    SyncServer

--- app/Http/Controllers/Assignments/UserController.php
+++ app/Http/Controllers/Assignments/UserController.php
@@ -276,7 +276,7 @@ public function push(Request $request)
             $time = str_replace(" ", "-", $time);
             $time = str_replace(":", "-", $time);
             $filename = $time.$filename;
-            $savefile = $destinationPath.$filename
+            $savefile = $destinationPath.$filename;
             dd($savefile);
             //Storage::move($final, 'hodor/newfile-name.jpg');
             //move file go to place
