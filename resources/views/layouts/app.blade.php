28c9a3fd29c65aac06bea6a2605a1307bac9a4e6
Colonel-Top <promsurinm@hotmail.com>
2018-01-14 13:30:50 +0700

    Try Footer Bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,13 +16,88 @@
         {
             padding-right: 20px;
         }
+        .footer-distributed{
+            background-color: #292c2f;
+            box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+            box-sizing: border-box;
+            width: 100%;
+            text-align: left;
+            font: normal 16px sans-serif;
+
+            padding: 45px 50px;
+            margin-top: 80px;
+        }
+
+        .footer-distributed .footer-left p{
+            color:  #8f9296;
+            font-size: 14px;
+            margin: 0;
+        }
+
+        /* Footer links */
+
+        .footer-distributed p.footer-links{
+            font-size:18px;
+            font-weight: bold;
+            color:  #ffffff;
+            margin: 0 0 10px;
+            padding: 0;
+        }
+
+        .footer-distributed p.footer-links a{
+            display:inline-block;
+            line-height: 1.8;
+            text-decoration: none;
+            color:  inherit;
+        }
+
+        .footer-distributed .footer-right{
+            float: right;
+            margin-top: 6px;
+            max-width: 180px;
+        }
+
+        .footer-distributed .footer-right a{
+            display: inline-block;
+            width: 35px;
+            height: 35px;
+            background-color:  #33383b;
+            border-radius: 2px;
+
+            font-size: 20px;
+            color: #ffffff;
+            text-align: center;
+            line-height: 35px;
+
+            margin-left: 3px;
+        }
+
+        /* If you don't want the footer to be responsive, remove these media queries */
+
+        @media (max-width: 600px) {
+
+            .footer-distributed .footer-left,
+            .footer-distributed .footer-right{
+                text-align: center;
+            }
+
+            .footer-distributed .footer-right{
+                float: none;
+                margin: 0 auto 20px;
+            }
+
+            .footer-distributed .footer-left p.footer-links{
+                line-height: 1.8;
+            }
+        }
+
     </style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-inverse navbar-fixed-top">
+        <nav class="navbar navbar-inverse navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
 
@@ -150,5 +225,115 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+        <footer class="footer-distributed">
+
+            <div class="footer-right">
+
+                <a href="https://www.facebook.com/colonel.pp"><i class="fa fa-facebook"></i></a>
+                <!--<a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>-->
+                <a href="https://github.com/Colonel-Top"><i class="fa fa-github"></i></a>
+
+            </div>
+
+            <div class="footer-left">
+
+                <p class="footer-links">
+                    @if(Auth::guest())
+
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                     
+                            
+                        @elseif(Auth::guard('admin')->check())
+                           
+                                <li class>
+                                    <a href="{{route('admin.dashboard')}}">Dashboard</a>
+                                </li>
+                                <li >
+                                    <a href="{{route('admin.courses.index')}}">Courses Manager</a>
+                                </li>
+                                <li>
+                                    <a href="{{route('admin.remarks.pickmode')}}">Remarks Management</a>
+                                </li>
+                                <li>
+                                    <a href="{{route('admin.register.show')}}">Assign New Administrator</a>
+                                </li>
+                          <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>@if(Auth::guard('admin')->check())
+                                        <a href="{{ route('admin.profile.request') }}"">
+                                            Edit Profile
+                                        </a>
+                                        @else
+                                            <a href="{{ route('user.profile.request') }}"">
+                                            Edit Profile
+                                        </a>
+                                        @endif
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @else
+                                
+                                <li class>
+                                    <a href="{{route('dashboard')}}">Dashboard</a>
+                                </li>
+                                <li >
+                                    <a href="{{route('user.courses.index')}}">Courses List</a>
+                                </li>
+                                <li >
+                                    <a href="{{route('user.courses.my')}}">My Courses</a>
+                                </li>
+                                <li>
+                                    
+                                <li>
+                                    <a href="{{route('user.grades.getcourse')}}">Remarks</a>
+                                </li>
+                                <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('user.profile.request') }}"">
+                                            Edit Profile
+                                        </a>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                         
+                       
+                         
+                        @endguest
+                </p>
+
+                <p>Company Name &copy; 2015</p>
+            </div>
+
+        </footer>
 </body>
 </html>
