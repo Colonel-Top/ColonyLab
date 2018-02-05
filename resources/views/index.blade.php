0aaf8e1a6dca732c1df7fa2f5f930c0d53622a63
Colonel-Top <promsurinm@hotmail.com>
2018-02-05 23:26:12 +0700

    Index Change Programming Kindle

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -228,7 +228,7 @@
                 </div>
                 </div>
                 <div class = "midupdown semititle typewriter">
-                    <Programming Kindle> Lab Practice System
+                    < Programming Kindle > Lab Practice System
                   
                     </div>
                   
