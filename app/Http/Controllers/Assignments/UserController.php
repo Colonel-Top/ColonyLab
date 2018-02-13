bed241b5eacd4660eac4630b60b758bab2ccb89a
Colonel-Top <promsurinm@hotmail.com>
2018-02-14 02:15:36 +0700

    Check getject

--- app/Http/Controllers/Assignments/UserController.php
+++ app/Http/Controllers/Assignments/UserController.php
@@ -261,7 +261,7 @@ public function push(Request $request)
             echo($restore);
             echo("<br>");
             echo($geter);
-            exit();
+            //exit();
             unlink($checkpath);
             unlink($getject);
 }
