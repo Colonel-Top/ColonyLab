f7ce830d03adb4f34f2435295770d302c2df1019
Colonel-Top <promsurinm@hotmail.com>
2018-02-14 01:48:45 +0700

    Check getject

--- app/Http/Controllers/Assignments/UserController.php
+++ app/Http/Controllers/Assignments/UserController.php
@@ -251,7 +251,7 @@ public function push(Request $request)
             
             $restore = File::get($asn->foutput);
             $getject = $destinationPath.$filename.'.txt';
-            dd($getject);
+           // dd($getject);
             $geter = File::get($getject);
             $whatsap = strcmp($restore, $geter);
             if($whatsap == 0)
